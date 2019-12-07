21c1a8db3d8785c20fc249c5bdf3257ebe797b6a
treem0 <travismolter@gmail.com>
2019-12-07 15:12:27 -0800

    Serving something useful

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,19 +1,22 @@
 var exec = require("child_process").exec;
 
-function start() {
+function start(response) {
     console.log("Request handler 'start' was called.");
-    var content = "empty";
 
-    exec("find /", function(error, stdout, stderr) {
-        content = stdout;
+    exec("find /",
+    { timeout: 10000, maxBuffer: 20000*1024 },
+    function(error, stdout, stderr) {
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write(stdout);
+        response.end();
     })
-    
-    return content;
 }
 
-function upload() {
+function upload(response) {
     console.log("Request handler 'upload' was called.");
-    return "Hello Upload";
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello Upload");
+    response.end();
 }
 
 exports.start = start;
